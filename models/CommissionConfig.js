042abbde9fb1aa3570279c3b399b6ce5c5c90f0e
Er.Mayank Pandey <mp04042007@gmail.com>
2026-06-06 12:00:33 +0530

    feat: create CommissionConfig model for managing energy sector pricing tiers

--- models/CommissionConfig.js
+++ models/CommissionConfig.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 module.exports = mongoose.model('CommissionConfig', new mongoose.Schema({
-  residential: { type: Number, default: 1000 },
-  commercial:  { type: Number, default: 5000 },
-  society:     { type: Number, default: 3000 },
-  offGrid:     { type: Number, default: 2000 },
+  residential: { type: Number, default: 1999 },
+  commercial:  { type: Number, default: 4999 },
+  society:     { type: Number, default: 4999 },
+  offGrid:     { type: Number, default: 4999 },
 }, { timestamps: true }))
